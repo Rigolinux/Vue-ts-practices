3b6edd70c55180b0fb5b6c3c6b43512132e196a8
rigolinux <rigoberto7567@gmail.com>
2023-07-11 21:23:47 -0600

    external functionality

--- src/views/DevExtremeTable.vue
+++ src/views/DevExtremeTable.vue
@@ -1,8 +1,34 @@
 <script setup lang="ts">
 
 import { users } from '@/helpers/Personas';
-import { DxColumn,DxDataGrid, DxPaging, } from 'devextreme-vue/data-grid';
+import { DxColumn,DxDataGrid, DxPaging,DxExport } from 'devextreme-vue/data-grid';
+import { exportDataGrid } from 'devextreme/excel_exporter';
+import { saveAs } from 'file-saver';
+import { Workbook } from 'exceljs';
+import { ref } from 'vue';
 
+
+
+const dataGrid = ref<any>(null);
+
+const onExporting = (e:any) => {
+      const workbook = new Workbook();
+      const worksheet = workbook.addWorksheet('Users');
+      console.log(e.component);
+      exportDataGrid({
+        component: e.component.instance,
+        worksheet: worksheet,
+        autoFilterEnabled: true
+      }).then(() => {
+        workbook.xlsx.writeBuffer().then((buffer) => {
+          saveAs(new Blob([buffer], { type: 'application/octet-stream' }), 'Users.xlsx');
+        });
+      });
+    };
+
+const setExport = () => {
+      onExporting({ component: dataGrid.value });
+    };
 
 
 
@@ -11,6 +37,8 @@ import { DxColumn,DxDataGrid, DxPaging, } from 'devextreme-vue/data-grid';
 
 <template>
 
+    <h1>DevExtreme Table</h1>
+    <button @click="setExport">Exportar</button>
     
     <DxDataGrid
     :data-source="users"
@@ -18,8 +46,10 @@ import { DxColumn,DxDataGrid, DxPaging, } from 'devextreme-vue/data-grid';
     :allow-column-reordering="true"
     :row-alternation-enabled="true"
     :show-borders="true"
-    
+    @exporting="onExporting"
+    ref="dataGrid"
     >
+    <DxExport :enabled="true" />
     <DxColumn data-field="id" caption="ID" :width="50"></DxColumn>
     <DxColumn data-field="nombre" caption="First Name"></DxColumn>
     <DxColumn data-field="correo" caption="correo"></DxColumn>
